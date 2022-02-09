d04bf42fdb0613c350f915ecad247e519142ed95
natrey28 <nreyes28@uic.edu>
2022-02-09 06:11:51 -0600

    delivery driver added

---
+++ app/src/main/java/com/example/cook2/driverMainActivity3.java
@@ -0,0 +1,29 @@
+package com.example.cook2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class driverMainActivity3 extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_3);
+        Button button = findViewById(R.id.backButton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), driverMainActivity2.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
