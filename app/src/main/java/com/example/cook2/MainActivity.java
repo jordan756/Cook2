76a6b73c728ffba6f201506a708fcaddcbee355a
wasayahmed <wahmed9@uic.edu>
2022-02-19 17:13:34 -0600

    Pending order part

--- app/src/main/java/com/example/cook2/MainActivity.java
+++ app/src/main/java/com/example/cook2/MainActivity.java
@@ -6,11 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.cook2.objects.Cook;
 import com.example.cook2.ui.login.LoginActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,18 +27,31 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton backButton = findViewById(R.id.floatingActionButton);
+        //FloatingActionButton backButton = findViewById(R.id.floatingActionButton);
 
+        ArrayList<String> arrayList;
+        ArrayAdapter<String> adapter;
 
+        ListView listView = (ListView) findViewById(R.id.listOrders);
+        String[] items = {"Example Order 1 (When we get database, we can remove these)", "Example Order 2", "Example Order 3"};
+        arrayList = new ArrayList<>(Arrays.asList(items));
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                view.setSelected(true);
+            }
+        });
         Cook testCook = (Cook) getIntent().getParcelableExtra("testCook");
 
         System.out.println(testCook.getFirstName() + " " + testCook.getLastName());
         System.out.println("size of list" + testCook.menu.size());
-        backButton.setOnClickListener(new View.OnClickListener() {
+        /*backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //System.print
                 System.out.println("floating button");
             }
-        });
+        });*/
     }
 }
