0f6bfcb2c60fd91712e272dd603bc0c13d0d7cef
cgamino <chrisguti415@gmail.com>
2022-03-13 21:26:43 -0500

    login push

---
+++ app/src/main/java/com/example/cook2/loginActivity.java
@@ -0,0 +1,99 @@
+package com.example.cook2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.widget.Button;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.example.cook2.objects.Cook;
+import com.example.cook2.objects.Person;
+import com.example.cook2.objects.Util;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import android.view.View;
+
+
+public class loginActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String email, password;
+    EditText emailInput;
+    EditText passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signIn(View v) {
+        emailInput = findViewById(R.id.username);
+        passwordInput = findViewById(R.id.password);
+        email = emailInput.getText().toString();
+        password = passwordInput.getText().toString();
+
+        if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+            emailInput.setError("Cannot be empty.");
+            passwordInput.setError("Cannot be empty.");
+        } else if (TextUtils.isEmpty(email)) {
+            emailInput.setError("Cannot be empty.");
+        } else if (TextUtils.isEmpty(password)) {
+            passwordInput.setError("Cannot be empty.");
+        } else {
+            //--------------
+            Log.d("firebaseQuery", "executing query: signIn(View v)");
+            db.collection("Person").whereEqualTo("email", email).whereEqualTo("password", password).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    String userType = document.getString("userType");
+                                    Person user = document.toObject(Person.class);
+                                    //String userType = user.getUserType();
+                                    Log.d("firebaseQuery", document.getId() + " => " + document.getData());
+//                                    Log.d("firebaseQuery", userType);
+//                                    Log.d("firebaseQuery", user.getUserType());
+
+                                    if (userType == "Cook") {
+                                        //Cook cook = document.toObject(Cook.class);
+                                        //String myUserType = cook.getUserType();
+                                        Cook cook = Util.getCook(user.getEmail()+user.getPassword(), db);
+
+                                    } else if (userType == "Customer") {
+
+                                    } else if (userType == "Driver") {
+
+                                    }
+
+                                    emailInput.setError("Try again.");
+                                    passwordInput.setError("Try again.");
+                                    return;
+                                }
+                            } else {
+                                Log.w("firebaseQuery", "Error getting documents.", task.getException());
+                                emailInput.setError("Try again.");
+                                passwordInput.setError("Try again.");
+                                return;
+                            }
+                        }
+                    });
+
+            emailInput.setError("Wrong username or password.");
+            passwordInput.setError("Wrong username or password.");
+            //------------
+        }
+    }
+
+    public void createAccount(View v) {
+        v.setEnabled(false);
+    }
+}
--- app/src/main/java/com/example/cook2/objects/Person.java
+++ app/src/main/java/com/example/cook2/objects/Person.java
@@ -2,7 +2,6 @@ package com.example.cook2.objects;
 
 import android.os.Parcel;
 
-
 public class Person  {
     protected String email;
     protected String password;
@@ -12,70 +11,79 @@ public class Person  {
     protected int numberOfRatings;
     protected String phoneNumber;
     protected String address; //maybe break into multiple pieces
-  //  protected String  docId;
+    protected String key;
+    protected String userType;
+    protected String userTypeKey;
+
+    public Person() {}
+
+
+    public String getUserTypeKey() {
+        return userTypeKey;
+    }
+    public String getUserType() {
+        return userType;
+    }
     public String getKey() {
-        return email + password;
+        return key;
     }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getPassword() {return password;}
-
     public String getFirstName() {
         return firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public double getCurrentRating() {
         return currentRating;
     }
-
     public int getNumberOfRatings() {
         return numberOfRatings;
     }
-
     public String getAddress() {
         return address;
     }
-
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public void setCurrentRating(double currentRating) {
         this.currentRating = currentRating;
     }
-
     public void setNumberOfRatings(int numberOfRatings) {
         this.numberOfRatings = numberOfRatings;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUserTypeKey(String userTypeKey) {
+        this.userTypeKey = userTypeKey;
+    }
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+    public void setKey(String key) {
+        this.key = key;
     }
 }
 
