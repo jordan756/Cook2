c71e686c69c20341312813df69684cce77beceb5
natrey28 <nreyes@uic.edu>
2022-03-24 22:27:11 -0500

    Driver class created, driver layout changed, icons added

--- app/src/main/java/com/example/cook2/driverMainActivity.java
+++ app/src/main/java/com/example/cook2/driverMainActivity.java
@@ -16,9 +16,9 @@ import java.security.Key;
 
 public class driverMainActivity extends AppCompatActivity {
 
-    EditText  firstName3, lastName3, phoneNumber3, emailAddress3, customerPassword2, vehicleM, vehicleColor, vehiclePlate;
-    Button cdeliveryButton, backButton, saveButton;
-    Switch status3, status;
+    EditText  firstName, lastName, phoneNumber, email, password, vMake, vColor, vPlate;
+    Button cdeliveryButton;
+    //Switch status3, status;
     TextView driverProfile, infoText;
 
     //shared preferences
@@ -30,89 +30,29 @@ public class driverMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driver_1);
 
         //initialize (xml)
-        firstName3 = findViewById(R.id.firstName3);
-        lastName3 = findViewById(R.id.lastName3);
-        phoneNumber3 = findViewById(R.id.phoneNumber3);
-        emailAddress3 = findViewById(R.id.emailAddress3);
-        customerPassword2 = findViewById(R.id.customerPassword2);
-        vehicleM = findViewById(R.id.vehicleM);
-        vehicleColor = findViewById(R.id.vehicleColor);
-        vehiclePlate = findViewById(R.id.vehiclePlate);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
 
-        cdeliveryButton = findViewById(R.id.cdeliveryButton);
-        backButton = findViewById(R.id.backButton);
-        saveButton = findViewById(R.id.saveButton);
+//        vMake = findViewById(R.id.vMake);
+//        vColor = findViewById(R.id.vColor);
+//        vPlate = findViewById(R.id.vPlate);
+//        cdeliveryButton = findViewById(R.id.cdeliveryButton);
 
-        status3 = findViewById(R.id.status3);
-        status = findViewById(R.id.status);
+
+//        status3 = findViewById(R.id.status3);
+//        status = findViewById(R.id.status);
 
         driverProfile = findViewById(R.id.driverProfile);
-        infoText = findViewById(R.id.infoText);
+        //infoText = findViewById(R.id.infoText);
         //carInformation = findViewById(R.id.carInformation);
 
         sharedPreferences = getSharedPreferences("SP_NAME", MODE_PRIVATE);
 
 
-        //save button
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //input data
 
-                String first = firstName3.getText().toString();
-                String last = lastName3.getText().toString();
-                int phone = Integer.parseInt(phoneNumber3.getText().toString().trim());
-                String email = emailAddress3.getText().toString();
-                String password = customerPassword2.getText().toString();
-                String vehiclem = vehicleM.getText().toString();
-                String vehiclec = vehicleColor.getText().toString();
-                String vehiclep= vehiclePlate.getText().toString();
-
-
-                //edit data (add new vehicle info)
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-
-                //putting the data in
-                editor.putString("FIRST", first);
-                editor.putString("LAST", last);
-                editor.putInt("PHONE", phone);
-                editor.putString("EMAIL", email);
-                editor.putString("PASSWORD", password);
-                editor.putString("VEHICLE", vehiclem);
-                editor.putString("VEHICLEC", vehiclec);
-                editor.putString("VEHICLEP", vehiclep);
-
-                //apply new changes made to info
-                editor.apply();
-
-            }
-
-        });
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //data from shared preferences (needs work)
-
-                String first = sharedPreferences.getString("FIRST", "");
-                String last = sharedPreferences.getString("LAST", "");
-                int phone = sharedPreferences.getInt("PHONE", 0);
-                String email = sharedPreferences.getString("EMAIL", "");
-                String password = sharedPreferences.getString("PASSWORD", "");
-                String vehiclem = sharedPreferences.getString("VEHICLE", "");
-                String vehiclec = sharedPreferences.getString("VEHICLEC", "");
-                String vehiclep = sharedPreferences.getString("VEHICLEP", "");
-
-                //show data (needs work)
-                infoText.setText("Name: " +first +last
-                        +"\nPhone Number:" +phone
-                        +"\nEmail: " +email
-                        +"\nPassword" +password
-                        +"\nVehicle Info: " +vehiclem +vehiclec +vehiclep);
-
-            }
-
-        });
 
     }
 }
