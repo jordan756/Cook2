96c59efeef30712cc13f66e18046f3b738a6b8a6
Jordan Nguyen <jtn3>
2022-03-26 17:37:18 -0500

    customer view menu

---
+++ app/src/main/java/com/example/cook2/CustmerViewMenu.java
@@ -0,0 +1,13 @@
+package com.example.cook2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustmerViewMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custmer_view_menu);
+    }
+}
