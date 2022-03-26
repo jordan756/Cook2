3c763d5f1f5fec9db4a92ee6a35786b8fec19e1b
cgamino <chrisguti415@gmail.com>
2022-03-26 01:06:09 -0500

    change register activities

--- app/src/main/java/com/example/cook2/registerActivity.java
+++ app/src/main/java/com/example/cook2/registerActivity.java
@@ -30,7 +30,7 @@ public class registerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+        setContentView(R.layout.activity_register);
     }
 
     public boolean getData() {
